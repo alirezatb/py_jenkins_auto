ea82a6833df3ed30082e4e4725c6abbc22e711e5
alirezatb <ar.tabebordbar@gmail.com>
2021-09-13 21:01:12 +1000

    add

--- python_multiply.py
+++ python_multiply.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 class multiply:
-    def times(self, input_1):
+    def times(self, input_2):
         set_value_1 = datetime.now().day
-        return str(input_1) + ' ' + str(set_value_1)
+        return str(input_2) + ' ' + str(set_value_1)
 
 if __name__=='__main__':
     ml = multiply()
